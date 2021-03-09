2f1f000ef72d36709ad8589105bbfcddb1083ae4
Joey Steigelman <jhs430@nyu.edu>
2021-03-09 15:13:56 +0100

    User can pull to refresh

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -13,16 +13,19 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     
     var posts = [PFObject]()
-    
+    var refreshControl: UIRefreshControl!
     
     @IBOutlet weak var tableView: UITableView!
 
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        
         // Do any additional setup after loading the view.
     }
     
@@ -41,6 +44,18 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
                 self.tableView.reloadData()
             }
         }
+    }
+    
+    @objc func onRefresh() {
+        run(after: 1) {
+            self.refreshControl.endRefreshing()
+        }
+    }
+    
+    // Implement the delay method
+    func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+        let queue = DispatchQueue.main
+        queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
     }
     
     
