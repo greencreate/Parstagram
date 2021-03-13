f5610b22e8c84dc48cbc8f0007c7e5d6ff33b195
Joey Steigelman <jhs430@nyu.edu>
2021-03-13 19:29:42 +0100

    User stays logged in across restarts, user can logout

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -22,9 +22,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         Parse.initialize(with: parseConfig)
         
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            // Override point for customization after application launch.
+            let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "pRBqMVl2FQenldWXKJqrQ39XoZq7FBviCHWYfMdG" // <- UPDATE
+                    $0.clientKey = "ZMGZOW4BsZZCwszYB6FXWUPOK5WI0kF3YWebQM7h" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+            
+            return true
+        }
+        
         return true
     }
 
+ 
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -122,4 +122,18 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     */
 
+    @IBAction func onLogoutButton(_ sender: Any) {
+        
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+
+        let delegate = self.view.window?.windowScene?.delegate as! SceneDelegate
+        delegate.window?.rootViewController = loginViewController
+        
+        
+    }
+    
+    
 }
